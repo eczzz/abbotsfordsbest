bb087707eccf033d3cbd84f994d27996393fe112
Bullfinch <86013174+eczzz@users.noreply.github.com>
2025-09-22 11:02:09 -0600

    Fix Astro adapter for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify(),
   output: 'server',
   vite: {
     optimizeDeps: {
